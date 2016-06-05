1133b56754fb0e83d9cc2ba16f68f2721a7a3196
Mathilda Hartnell <nekkoru@gmail.com>
2016-06-05 19:11:35 +0200

    fast bugfix

--- rattletrap.py
+++ rattletrap.py
@@ -17,25 +17,32 @@ REALNAME = "Rattletrap the Clockwerk"
 NICKSERV = "changeme" # optional, if your network requires you to register
 CHANNEL = "#dota"
 
-STEAMID = re.compile("STEAM_0:0:\d{8,}")
 
 s = socket.socket()
 data_file = open("data.json", "r+")
 ids = json.load(data_file)
 api = dota2api.Initialise("E337281DA466818041F26B4AD42F7C4A")
 
+HEROES = api.get_heroes()[heroes]
+
 def findmatch(id):
   try:
     match = api.get_match_details(match_id=id)
     #print(json.dumps(match, sort_keys=True, indent=2))
     return match
+  
+  except: dota2api.src.exceptions.APIError:
+    say("No match specified")
 
   except dota2api.src.exceptions.APITimeoutError:
     say("503 API unavailable.")
 
-def parse_match(id):
+def parse_match(id, player_id=False):
   match = findmatch(id)
   say("Match id {0}, {1} victory. Dotabuff link: http://www.dotabuff.com/matches/{2}".format(match["match_id"], "Radiant" if match["radiant_win"] else "Dire", match["match_id"]))
+  
+  if player_id==True:
+    if player_id in match[players]
 
 
 def say(message):
